927df0353db442d9478dcd48349e407c5a9ca399
nicanororlando <nicanororlando@gmail.com>
2022-03-11 19:23:07 -0300

    agrego ej4

--- bootstrap-4/ristorante-confusion/script.js
+++ bootstrap-4/ristorante-confusion/script.js
@@ -1,4 +1,26 @@
+// Tooltip //
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
      return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+// play | pause Carousel //
+var mycarousel = document.querySelector('#mycarousel');
+var carouselButton = document.getElementById("carouselButton");
+var carousel = new bootstrap.Carousel(mycarousel, {
+    interval: 1000
+});
+carouselButton.addEventListener("click", function () {
+    if(carouselButton.firstElementChild.classList.contains("fa-pause")){
+        carouselButton.firstElementChild.classList.remove("fa-pause");
+        carouselButton.firstElementChild.classList.add("fa-play");
+        carousel.pause();
+    }
+    else if(carouselButton.firstElementChild.classList.contains("fa-play")){
+        carouselButton.firstElementChild.classList.remove("fa-play");
+        carouselButton.firstElementChild.classList.add("fa-pause");
+        carousel.cycle();
+    }
+});
+
+
